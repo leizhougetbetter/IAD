8d851965529ba6da9a128b9e5d1906d864fb5f5a
Lei Zhou <weifengzhl2012@gmail.com>
2018-05-01 23:27:17 +0800

    Update avgAbundance_a_0.m

--- avgAbundance_a_0.m
+++ avgAbundance_a_0.m
@@ -1,30 +1,41 @@
 clear
 clc
 
-a0 = 1.6 : 0.1 : 2.4;
-beta = 0.01;
 
-
-meanA_A0 = [];
-for indexA0 = 1 : length(a0)
-    data = importdata(['numA_Dist_a0_' num2str(a0(indexA0),'%4.2f')]);
-    meanARepeat = [];
-    for iRepeat = 1 : 200
-        dataNow = data(iRepeat,:);
-        numAUnique = unique(find(dataNow>0));
-        timesState = dataNow(numAUnique);
-        probState = timesState * 1.0 / sum(dataNow);
-        meanA = sum((numAUnique-1) .* probState)/100;
-        meanARepeat = [meanARepeat, meanA];
-    end
-    meanA_A0 = [meanA_A0; mean(meanARepeat)];
-end
+figure; 
 sizeMarker = 10;
-h1 = plot(a0,meanA_A0,'d','MarkerSize',sizeMarker, 'linewidth',2);
+selectionIntensity = 0.005;
+
+a0Array = 1.6 : 0.1 : 2.4;
+xPlot = 1.55 : 0.1 : 2.55;
+
+meanA_a0_OneDist = [];
+for a0 = 1.6 : 0.1 : 2.4
+    
+    meanA_a0_Array = [];
+    folder = ['a0-' num2str(a0,'%3.1f') '\'];
+    
+    for iRepeat = 1 : 3
+        
+        wholeDist = importdata([folder 'numA_Strategy_Dist_repeat_' num2str(iRepeat)]);
+        
+        probDist = wholeDist /  sum(wholeDist);
+        avgA = sum((0:1:1000)/1000 .* probDist);
+        meanA_a0_Array  = [meanA_a0_Array; avgA];
+    end
+    meanA_a0_OneDist = [meanA_a0_OneDist; mean(meanA_a0_Array)];
+end
+
+plot(a0Array,meanA_a0_OneDist,'o','markersize',sizeMarker,'linewidth',1.5)
 hold on;
-plot([1.55,2.45],[0.5, 0.5],'k--','linewidth',2)
-h3 = plot([1.55,2.45], 0.5+1/2^5*([1.55,2.45]-2)*beta,'r-','linewidth',2);
-legend([h1,h3],{'Simulation','Analytical'})
-title('\beta = 0.01, d = 3')
+
+plot(xPlot, 0.5+1/2^5*(xPlot-2)*selectionIntensity,'r-', 'linewidth',2)
+plot([1.55, 2.45], [0.5,0.5],'k--',  'linewidth',2)
 xlim([1.55,2.45])
-ylim([0.4998, 0.5002])
+ylim([0.49991,0.50009])
+axis square
+
+legend({'Simulation', 'Analytical'}, 'location','southeast', 'fontsize',11);
+title('Power-law distributed aspirations, SF')
+ylabel('Average abundance of strategy A', 'fontsize', 16)
+xlabel('$$a_0$$','Interpreter','latex','fontsize', 16)
